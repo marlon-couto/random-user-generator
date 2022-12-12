09ab60072e3e40fb3447edd93c8fcb7ab5f2de25
Marlon Couto <marloncouto.dev@gmail.com>
2022-12-12 13:04:24 -0300

    Modifica "App.js" para componente de classe
    Também adiciona estado e elementos para iniciar a criação da lógica de
    renderização condicional.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PersonDetails from './components/PersonDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      user: {},
+    };
+  }
+  
+  render() {
+    const { loading, user } = this.state;
+    const { name, email, picture, dob } = user;
+
+    const loadingElement = <span>Carregando...</span>;
+
+    return (
+      <div>
+        {loading ? (
+          loadingElement
+        ) : (
+          <PersonDetails
+            picture={picture}
+            personName={`${name.first} ${name.last}`}
+            email={email}
+            age={dob.age}
+          />
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
